3923280e3fe13b94ebec9d501e5f6684a648e38e
Jason Foord <jf@tag.com.au>
2022-06-02 16:23:16 +0930

    update structure

--- src/info.lua
+++ src/info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "TAG~Help Wizard",
   Version = "2.1",
-  BuildVersion = "2.2.0.1",
+  BuildVersion = "2.2.0.3",
   Id = "1c3e2ee3-a45f-4ffb-b894-8ea26a3d385b",
   Author = "Jason Foord (TAG)",
   Description = "Configure help issues that can assist your users with resolving problems."
